807c80228e5faf7cef32de54b99410251fa5f057
GabrielPereiraPinto <arkanjogabriel7@hotmail.com>
2018-09-22 02:21:01 -0300

    Simulação Finalizada

--- Simulacao2/Simulacao_Aeroporto/Simulacao_Aeroporto/BagagemPessoa.cs
+++ Simulacao2/Simulacao_Aeroporto/Simulacao_Aeroporto/BagagemPessoa.cs
@@ -8,16 +8,95 @@ namespace Simulacao_Aeroporto
 {
     public class BagagemPessoa
     {
-        public float Bagagem { get; set; }
-        public float BagagemMao { get; set; }
+        public int Bagagem { get; set; }
+        public int BagagemMao { get; set; }
+        public int TempoUsuario { get; set; }
+        public bool DespacharBagagemMao { get; set; }
+        public bool BagagemExtra { get; set; }
 
-        public BagagemPessoa(float bagagem, float bagagemMao)
+        public BagagemPessoa(int bagagem, int bagagemMao)
         {
             this.Bagagem = bagagem;
             this.BagagemMao = bagagemMao;
+            this.TempoUsuario = 0;
+            this.DespacharBagagemMao = false;
+            this.BagagemExtra = false;
         }
 
-        public BagagemPessoa() { }
+        public BagagemPessoa()
+        {
+            this.TempoUsuario = 0;
+            this.DespacharBagagemMao = false;
+            this.BagagemExtra = false;
+        }
+
+        public int PesagemBagagem()
+        {
+            if (Bagagem == 0)
+                return 0;
+
+            int tempoTotalPesagem = 0;
+            var pesagemBagagem = new Random().Next(5, 10);
+            var acertoPesoExtra = new Random().Next(5, 10);
+
+            tempoTotalPesagem += pesagemBagagem;
+
+            if (Bagagem > 23)
+            {
+                BagagemExtra = true;
+                tempoTotalPesagem += acertoPesoExtra;
+            }
+
+            TempoUsuario += tempoTotalPesagem;
+            return tempoTotalPesagem;
+        }
+
+        public int PesagemBagagemMao()
+        {
+            if (BagagemMao == 0)
+                return 0;
+
+            int tempoTotalPesagemMao = 0;
+
+            var random = new Random().Next(0, 100);
+            var pesagemBagagemMao = new Random().Next(5, 10);
+            var acertoPesoExtra = new Random().Next(5, 10);
+
+            if (random < 70)
+            {
+                tempoTotalPesagemMao += pesagemBagagemMao;
+
+                if (BagagemMao > 10)
+                {
+                    DespacharBagagemMao = true;
+                    tempoTotalPesagemMao += acertoPesoExtra;
+                }
+            }
+
+            TempoUsuario += tempoTotalPesagemMao;
+            return tempoTotalPesagemMao;
+        }
+
+        public int TempoPortao(int portao)
+        {
+            int tempoPortao = 0;
+            switch (portao)
+            {
+                case 1:
+                case 2:
+                    tempoPortao += 10;
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    tempoPortao += 15;
+                    break;
+            }
+
+            TempoUsuario += tempoPortao;
+            return tempoPortao;
+        }
 
     }
 }
--- Simulacao2/Simulacao_Aeroporto/Simulacao_Aeroporto/Program.cs
+++ Simulacao2/Simulacao_Aeroporto/Simulacao_Aeroporto/Program.cs
@@ -8,36 +8,82 @@ namespace Simulacao_Aeroporto
 {
     class Program
     {
-
-        
         static void Main(string[] args)
         {
 
             var usuario = new BagagemPessoa();
 
-
             Console.WriteLine("===========================================+++++++++=============");
             Console.WriteLine("============================BEM VINDO============================");
             Console.WriteLine("=================================================================");
             Console.WriteLine("Para Simular seu tempo gasto em um aeroporto, por favor digite:");
+
             Console.WriteLine("O Peso da sua bagagem:");
-            usuario.Bagagem = float.Parse(Console.ReadLine());
+            usuario.Bagagem = Int32.Parse(Console.ReadLine());
+
             Console.WriteLine("O Peso da sua bagagem de mão:");
-            usuario.BagagemMao = float.Parse(Console.ReadLine());
+            usuario.BagagemMao = Int32.Parse(Console.ReadLine());
 
+            Console.WriteLine("Você acaba de entrar na fila para pesar suas malas e fazer check-in");
 
-           
+            var filaBagagem = geraFila(12);
+            Console.WriteLine($"Atualmente existem {filaBagagem.Count()} pessoas na sua frente");
 
+            int tempoFila = 0;
+            foreach (var pessoa in filaBagagem)
+            {
+                pessoa.PesagemBagagem();
+                pessoa.PesagemBagagemMao();
+                tempoFila += pessoa.TempoUsuario;
+            }
+            Console.WriteLine($"Foram necessários {tempoFila} minutos para que todas pessoas a sua frente fossem atendidas");
 
+            usuario.TempoUsuario += tempoFila;
+            int tempoPesagem = 0;
+            tempoPesagem += usuario.PesagemBagagem();
+            tempoPesagem += usuario.PesagemBagagemMao();
+
+            if (usuario.BagagemExtra)
+                Console.WriteLine("Sua Bagagem estava acima do peso de 23 kgs, foi necessário pagar pelo peso extra");
+
+            if (usuario.DespacharBagagemMao)
+                Console.WriteLine("Sua bagagem de mão estava acima do limite de 10 kgs e precisou ser despachada");
+            
+            Console.WriteLine($"Foram necessários {tempoPesagem} minutos para terminar de pesar a suas bagagens e fazer seu check-in");
+
+            var pessoasInspecao = new Random().Next(0, 5);
+            usuario.TempoUsuario += pessoasInspecao * 4;
+            Console.WriteLine($"Você está se encaminhando para fila de inspeção de bagagens, atualmente existem {pessoasInspecao} pessoas e levará {pessoasInspecao*4} minutos para você prosseguir");
+
+            var inspecaoBagagem = new Random().Next(0, 100);
+            if (inspecaoBagagem > 80)
+            {
+                Console.WriteLine($"Foi Necessário revistar a sua bagagem de mão, isso levou mais 5 minutos");
+                usuario.TempoUsuario += 5;
+            }
+
+            var portao = new Random().Next(1, 5);
+            var tempoPortao = usuario.TempoPortao(portao);
+
+            Console.WriteLine($"O Seu portão de entrada será o portão de número  {portao}, que fica a {tempoPortao} minutos de distancia");
+
+            if(portao == 5)
+            {               
+                Console.WriteLine($"O Portão de número 5 necessita de Onibus até o avião, isso levará mais 10 minutos");
+                usuario.TempoUsuario += 10;
+            }
+
+            Console.WriteLine($"Você Embarcou em seu Avião");
+            Console.WriteLine($"Foram necessários {usuario.TempoUsuario} minutos para que você termina-se todo o processo.");
         }
 
-        static List<BagagemPessoa> geraFila()
+        static List<BagagemPessoa> geraFila(int tamanhoMax)
         {
-            var filaRandom = new Random().Next(0, 20);
+            var filaRandom = new Random().Next(0, tamanhoMax);
             var fila = new List<BagagemPessoa>();
             for (int i = 0; i < filaRandom; i++)
             {
-                var bagagem = new Random().Next(0, 30);
+                var bagagem = new Random().Next(5, 30);
                 var bagagemMao = new Random().Next(0, 15);
 
                 var usuarioFila = new BagagemPessoa(bagagem, bagagemMao);
